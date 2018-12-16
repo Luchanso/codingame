499bcd47dc8ccefdedeb8a6b9505f241c4ab35d8
Oleg Loutchansky <luchansodev@gmail.com>
2018-12-16 18:03:03 +0300

    feat(*): initi gravity-tumlber

---
+++ gravity-tumbler/index.js
@@ -0,0 +1,51 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+/**
+ * Поворот земли должен осуществляться против часовой стрелки
+ */
+
+var inputs = readline().split(' ');
+const width = parseInt(inputs[0]);
+const height = parseInt(inputs[1]);
+const count = parseInt(readline());
+for (let i = 0; i < height; i++) {
+    const raster = readline();
+}
+
+// Write an action using print()
+// To debug: printErr('Debug messages...');
+
+print('...');
+print('write ###');
+/*
+
+17 5
+1
+.................
+.................
+...##...###..#...
+.####..#####.###.
+#################
+
+....#
+....#
+....#
+....#
+....#
+...##
+...##
+...##
+...##
+...##
+...##
+..###
+..###
+..###
+..###
+..###
+..###
+
+*/
